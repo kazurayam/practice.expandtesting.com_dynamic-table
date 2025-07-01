9e996041f1449ed4f215cdc886ded62801b1a9a1
kazurayam <kazuaki.urayama@gmail.com>
2025-07-02 08:34:56 +0900

    add comments in the TC1

--- Scripts/TC1/Script1751381292041.groovy
+++ Scripts/TC1/Script1751381292041.groovy
@@ -4,6 +4,11 @@ import com.kms.katalon.core.testobject.ConditionType
 import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
+/**
+ * A proposal to the question at
+ * https://forum.katalon.com/t/how-handle-a-dynamic-webtable-in-katalon-studio-what-are-the-ways-we-can-access-the-table/177118/ 
+ */
+
 TestObject makeTestObject(String id, String xpath) {
 	TestObject tobj = new TestObject(id)
 	tobj.addProperty("xpath", ConditionType.EQUALS, xpath)
@@ -20,33 +25,41 @@ int findIndexOfWebElementByText(List<WebElement> list, String text) {
 	return v
 }
 
+// The HTML of this URL contains a terrible <table>. 
+// It changes the order of rows and columns
+// every time the page is retrieved.
 String url = 'https://practice.expandtesting.com/dynamic-table'
-
 WebUI.openBrowser('')
 WebUI.navigateToUrl(url)
-WebUI.delay(3)
 
+// wait for the page is loaded on the browser viewport
 WebUI.verifyElementPresent(makeTestObject('coreTable', 'id("core")//table'), 10)
 
+// get the list of <th>Name</th>...<th>CPU</th>... as the column titles
 List<WebElement> listOfTdInThead = 
 	WebUI.findWebElements(
 		makeTestObject('listofTdInThead', 'id("core")//table/thead/tr/th'), 10)
 
+// find the index of "Name" column
 int nameColumnIndex = findIndexOfWebElementByText(listOfTdInThead, 'Name')
 println "nameColumnIndex=${nameColumnIndex}"
 
+// find the index of "CPU" column
 int cpuColumnIndex = findIndexOfWebElementByText(listOfTdInThead, 'CPU')
 println "cpuColumnIndex=${cpuColumnIndex}"
 
+// get the list of <td>Chrome</td>...<td>Firefox</td>... as the browser names
 List<WebElement> listOfTdInNameColumn = 
 	WebUI.findWebElements(
 		makeTestObject('listOfTdInNameColumn', "id('core')//table/tbody/tr/td[${nameColumnIndex + 1}]"), 10)
 
+// find the index of "Chrome" row
 int chromeRowIndex = findIndexOfWebElementByText(listOfTdInNameColumn, 'Chrome')
 println "chromeRowIndex=${chromeRowIndex}" 
 
+// select the <td> of "Chrome"x"CPU", get the content text
 String textOfChromeCPU = 
 	WebUI.getText(makeTestObject('chromeCPU', "id('core')//table/tbody/tr[${chromeRowIndex + 1}]/td[${cpuColumnIndex + 1}]"))
-println "textofChromeCPU=${textOfChromeCPU}"
+println "textOfChromeCPU=${textOfChromeCPU}"
 
 WebUI.closeBrowser()
